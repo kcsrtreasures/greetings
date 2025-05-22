8b07cfa6b2bcedbaa302a3fd5f3aee03e9bfd357
kcsrtreasures <kcsrdev@gmail.com>
2025-05-22 14:04:57 +0800

    vite config and index

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/greetings/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/index.html'
+    }
+  }
 })
